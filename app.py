07c79f845bef22f1340ef56780a5d39f309b44f1
sowmya-gajula <112384286+sowmya-gajula@users.noreply.github.com>
2023-12-21 01:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 import streamlit as st
 import pickle
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 
+# Load the model
 with open("model.sav", "rb") as file:
-    random_model = pickle.load(file)
-with open("scaler.sav", "rb") as file:
-    scaler = pickle.load(file)
+    model = pickle.load(file)
 
-scaler = MinMaxScaler()
-
-st.title("Health Prediction App")
-st.subheader("Enter Health Parameters:")
-
+# Collect input parameters from the user
 age = st.number_input("Age", min_value=0, max_value=120, step=1)
 systolic_bp = st.number_input("Systolic Blood Pressure", min_value=0, max_value=300, step=1)
 diastolic_bp = st.number_input("Diastolic Blood Pressure", min_value=0, max_value=200, step=1)
@@ -20,33 +14,23 @@ bs = st.number_input("Blood Sugar", min_value=0, max_value=300, step=1)
 body_temp = st.number_input("Body Temperature", min_value=30.0, max_value=43.0, step=0.1)
 heart_rate = st.number_input("Heart Rate", min_value=0, max_value=200, step=1)
 
-if st.button("Predict"):
-    input_data = pd.DataFrame({
-        'Age': [age],
-        'SystolicBP': [systolic_bp],
-        'DiastolicBP': [diastolic_bp],
-        'BS': [bs],
-        'BodyTemp': [body_temp],
-        'HeartRate': [heart_rate]
-    })
+# Create a DataFrame with the user input
+input_data = pd.DataFrame({
+    'Age': [age],
+    'SystolicBP': [systolic_bp],
+    'DiastolicBP': [diastolic_bp],
+    'BS': [bs],
+    'BodyTemp': [body_temp],
+    'HeartRate': [heart_rate]
+})
 
-    # Scale the input features
-    scaled_input_data = scaler.transform(input_data)
+# Use the same scaler object from the model
+scaler = model.named_steps['scaler']  # Adjust based on the actual name used in your pipeline
 
-    # Make prediction
-    prediction = model.predict(scaled_input_data)[0]
+# Scale the input features
+scaled_input_data = scaler.transform(input_data)
 
-    # Define threshold values for low, medium, and high
-    low_threshold = 0.33
-    high_threshold = 0.66
+# Make predictions using the model
+prediction = model.predict(scaled_input_data)[0]
 
-    # Map the prediction to discrete categories
-    if prediction <= low_threshold:
-        category = "Low"
-    elif low_threshold < prediction <= high_threshold:
-        category = "Medium"
-    else:
-        category = "High"
-
-    st.header("Prediction:")
-    st.subheader(f"The predicted health category is: {category}")
+# Rest of your code for displaying the prediction
